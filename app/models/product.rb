90c96bfdc8fd2965ad626d8d271c314a2e28b754
Matthew Anderson <manderson@onehub.com>
2009-01-24 11:55:45 -0800

    End of Chapter 7, fixing some tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,14 @@ class Product < ActiveRecord::Base
   end
   
   # validation stuff...
-  validates_presence_of :title, :description, :image_url
+  validates_presence_of :title, :description, :image_url, :price
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
   validates_uniqueness_of :title
   validates_format_of :image_url,
-                      :with => %r{\.(gif|jpg|png)$}i,
-                      :message => 'must be a URL for a GIF, JPG or PNG image.'
+                      :with    => %r{\.(gif|jpg|png)$}i,
+                      :message => 'must be a URL for a GIF, JPG ' +
+                                  'or PNG image.'
 
   protected
   
