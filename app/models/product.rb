80ef35b2e7c382e06461110de07823a553137386
Matthew Anderson <manderson@onehub.com>
2009-01-25 12:31:45 -0800

    Removing logs

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,13 +5,12 @@ class Product < ActiveRecord::Base
   
   # validation stuff...
   validates_presence_of :title, :description, :image_url, :price
-  validates_numericality_of :price
-  validate :price_must_be_at_least_a_cent
   validates_uniqueness_of :title
   validates_format_of :image_url,
                       :with    => %r{\.(gif|jpg|png)$}i,
-                      :message => 'must be a URL for a GIF, JPG ' +
-                                  'or PNG image.'
+                      :message => 'must be a URL for a GIF, JPG or PNG image.'
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
 
   protected
   
