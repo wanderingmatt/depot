9ad73b6f31bc32f17118b852775f9ffed6438c4d
Matthew Anderson <manderson@onehub.com>
2009-01-27 19:16:16 -0800

    All tests pass and price displays in dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
-class Product < ActiveRecord::Base
+class Product < ActiveRecord::Base  
+  before_save :price_in_dollars
+  
+  def price_in_dollars
+    self.price / 100.0 unless self.price == nil
+  end
+  
+  def price_in_dollars=(new_price)
+    self.price = (new_price.to_f * 100).to_i
+  end
+  
   def self.find_products_for_sale 
     find(:all, :order => 'title') 
   end
@@ -13,7 +23,7 @@ class Product < ActiveRecord::Base
   validate :price_must_be_at_least_a_cent
 
   protected
-  
+
   def price_must_be_at_least_a_cent
     errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
   end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -20,7 +20,7 @@ class ProductsControllerTest < ActionController::TestCase
       :name => 'product[image_url]'
     }
     assert_tag :tag => 'input', :attributes => {
-      :name => 'product[price]'
+      :name => 'product[price_in_dollars]'
     }
   end
 
@@ -54,7 +54,7 @@ class ProductsControllerTest < ActionController::TestCase
       :name => 'product[image_url]'
     }
     assert_tag :tag => 'input', :attributes => {
-      :name => 'product[price]'
+      :name => 'product[price_in_dollars]'
     }
     assert_response :success
   end
