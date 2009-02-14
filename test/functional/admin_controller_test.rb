4c9c4aced34c057d4043708598b95a8dde67a6ca
Matthew Anderson <manderson@onehub.com>
2009-02-14 11:40:52 -0800

    Tests for login and logout with exceptions

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,15 +1,52 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  test "needs authentication" do
+  def setup
+    @user = users(:one)
+  end
+
+  test "requires authentication" do
     get :index
     assert_response :redirect
     assert_redirected_to :controller => 'admin', :action => 'login'
   end
 
-  test "i am logged in" do
+  test "is logged in" do
     @request.session[:user_id] = users(:one).id
     get :index
     assert_response :success
+  end
+
+  test "logs user in" do
+    post :login, :name => @user.name, :password => 'coolestxman'
+    assert_response :redirect
+    assert_redirected_to :action => 'index'
+    assert_equal @user.id, session[:user_id]
+  end
+
+  test "logs user in and remembers referring uri" do
+    uri = 'http://localhost:3000/users'
+    @request.session[:original_uri] = uri
+    post :login, :name => @user.name, :password => 'coolestxman'
+    assert_nil @request.session[:original_uri]
+    assert_response :redirect
+    assert_redirected_to uri
+  end
+  
+  # test "login handles bad user" do
+  # end
+  # 
+  # test "login handles bad password" do
+  #   post :login, :info => { :name => @user.name, :password => 'jank' }
+  # end  
+
+  test "logs user out" do
+    post :login, :name => @user.name, :password => 'coolestxman'
+    @request.session[:user_id] = users(:one).id
+    post :logout
+    assert_nil @request.session[:user_id]
+    assert_response :redirect
+    assert_redirected_to :action => 'login'
+    assert flash[:notice]
   end
 end
