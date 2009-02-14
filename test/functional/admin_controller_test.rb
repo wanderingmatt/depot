7d247c0563865d95b640e2b73bd03507f4f2d5b8
Matthew Anderson <manderson@onehub.com>
2009-02-14 12:01:51 -0800

    Pushing broken tests for invalid logins for Aaron to look at

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -7,6 +7,7 @@ class AdminControllerTest < ActionController::TestCase
 
   test "requires authentication" do
     get :index
+    
     assert_response :redirect
     assert_redirected_to :controller => 'admin', :action => 'login'
   end
@@ -14,11 +15,13 @@ class AdminControllerTest < ActionController::TestCase
   test "is logged in" do
     @request.session[:user_id] = users(:one).id
     get :index
+    
     assert_response :success
   end
 
   test "logs user in" do
     post :login, :name => @user.name, :password => 'coolestxman'
+    
     assert_response :redirect
     assert_redirected_to :action => 'index'
     assert_equal @user.id, session[:user_id]
@@ -28,22 +31,33 @@ class AdminControllerTest < ActionController::TestCase
     uri = 'http://localhost:3000/users'
     @request.session[:original_uri] = uri
     post :login, :name => @user.name, :password => 'coolestxman'
+    
     assert_nil @request.session[:original_uri]
     assert_response :redirect
     assert_redirected_to uri
   end
-  
-  # test "login handles bad user" do
+
+  ## Can't figure these out - awaiting feedback from Aaron
+  #
+  # test "login handles invalid user name" do
+  #   post :login, :name => '', :password => 'coolestxman'
+  # 
+  #   assert assigns(:admin).errors.on :name
+  #   assert flash[:notice]
   # end
   # 
-  # test "login handles bad password" do
-  #   post :login, :info => { :name => @user.name, :password => 'jank' }
-  # end  
-
+  # test "login handles invalid password" do
+  #   post :login, :name => @user.name, :password => 'jank'
+  # 
+  #   assert_not_nil assigns(:admin).errors.on :password
+  #   assert flash[:notice]
+  # end
+  
   test "logs user out" do
     post :login, :name => @user.name, :password => 'coolestxman'
     @request.session[:user_id] = users(:one).id
     post :logout
+    
     assert_nil @request.session[:user_id]
     assert_response :redirect
     assert_redirected_to :action => 'login'
