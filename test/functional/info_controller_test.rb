0e9400605683ad141fa5568f104cb56f55543f92
Matthew Anderson <manderson@onehub.com>
2009-02-16 19:31:19 -0800

    Finished Chapter 14 + added an XML test

--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
+require 'nokogiri'
 
 class InfoControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "who_bought returns XML" do
+    get :who_bought, :id => products(:one).id, :format => 'xml'
+    doc = Nokogiri::XML(@response.body)
+    
+    assert_response :success
+    puts doc
+    assert_tag :tag => 'title', :content => products(:one).title
   end
 end
