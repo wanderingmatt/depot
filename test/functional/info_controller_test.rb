096ab468f000f4f3e428fbf34fb30b387ef15d32
Matthew Anderson <manderson@onehub.com>
2009-02-17 22:23:55 -0800

    Finished testing all who_bought response types

--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,12 +1,36 @@
 require 'test_helper'
 require 'nokogiri'
+require 'json'
 
 class InfoControllerTest < ActionController::TestCase
-  test "who_bought returns XML" do
+  test "who_bought returns correct HTML by default" do
+    get :who_bought, :id => products(:one).id
+    
+    assert_response :success
+    assert_tag :tag => 'h3', :content => "People Who Bought #{products(:one).title}"
+  end
+  
+  test "who_bought returns correct XML" do
     get :who_bought, :id => products(:one).id, :format => 'xml'
     doc = Nokogiri::XML(@response.body)
     
     assert_response :success
     assert_tag :tag => 'title', :content => products(:one).title
+  end
+  
+  test "who_bought returns correct ATOM" do
+    get :who_bought, :id => products(:one).id, :format => 'atom'
+    doc = Nokogiri::XML(@response.body)
+    
+    assert_response :success
+    assert_tag :tag => 'title', :content => "Who bought #{products(:one).title}"
+  end
+  
+  test "who_bought returns correct JSON" do
+    get :who_bought, :id => products(:one).id, :format => 'json'
+    doc = ActiveSupport::JSON(@response.body)
+    
+    assert_response :success
+    assert_equal products(:one).title, doc['product']['title']
   end
 end
